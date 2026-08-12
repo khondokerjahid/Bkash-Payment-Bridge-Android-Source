c5e82b0f968b27ba71f28d3615b22b12483c7b35
Khondoker Jahid <72726791+khondokerjahid@users.noreply.github.com>
2026-08-12 23:45:34 +0600

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jahid.bkashverify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.3"
     }
 
     buildFeatures {
