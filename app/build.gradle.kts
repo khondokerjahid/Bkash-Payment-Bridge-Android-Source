9f2f0fb45635c93c718d0c6fa1af99088c93c00b
Khondoker Jahid <72726791+khondokerjahid@users.noreply.github.com>
2026-08-13 11:01:16 +0600

    v2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jahid.bkashverify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.5"
+        versionCode = 8
+        versionName = "2.6"
     }
 
     buildFeatures {
